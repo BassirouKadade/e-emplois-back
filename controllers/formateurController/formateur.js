9d8abf78923d91019c640ec4e4b195a2d62e4135
Bassirou Kadade <bassiroukadade1@gmail.com>
2024-05-07 01:28:36 +0100

    le formulaire de modifcatin des formateurs

--- controllers/formateurController/formateur.js
+++ controllers/formateurController/formateur.js
@@ -59,6 +59,60 @@ const formateurController = {
       response.status(500).send('Erreur lors de la récupération des formateurs');
     }
   },
+ supprimer: async  (request, response)=>  {
+  try {
+    console.log( request.params.ids)
+    const formateurIds = request.params.ids.split('-');
+    console.log(formateurIds)
+    await Formateur.destroy({
+        where: {
+            id: formateurIds
+        }
+    });
+
+    response.status(200).json({ message: 'Formateurs supprimés avec succès' });
+} catch (error) {
+    console.error(error);
+    response.status(500).send('Erreur lors de la suppression des formateurs');
+}
+}
+,
+update: async (request, response) => {
+  try {
+    const { nom, prenom, metier, email, matricule } = request.body;
+    
+    const errorServer = {};
+
+    // Vérifier si tous les champs requis sont fournis
+    if (!nom || !prenom || !metier || !email || !matricule) {
+      errorServer.error = 'Tous les champs sont obligatoires';
+      return response.status(400).json(errorServer);
+    }
+
+    // Vérifier si un formateur avec le même email existe déjà
+    const formateurExist = await Formateur.findOne({ where: { email } });
+    if (formateurExist && formateurExist.matricule !== matricule) {
+      errorServer.existeEMail = "Un formateur avec cet email existe déjà";
+      return response.status(400).json({m:"poiuy"});
+    }
+
+    // Mettre à jour les informations du formateur
+    const formateur = await Formateur.findOne({ where: { matricule } });
+    if (!formateur) {
+      return response.status(404).json({ error: "Formateur non trouvé" });
+    }
+
+    await formateur.update({ nom, prenom, metier, email });
+
+    response.status(200).json({ success: "Formateur mis à jour avec succès" });
+  } catch (error) {
+    console.error(error);
+    response.status(500).send('Erreur lors de la mise à jour du formateur');
+  }
+}
+
+
+
 };
 
 module.exports = formateurController;
--- routes/formateurRoute.js
+++ routes/formateurRoute.js
@@ -1,7 +1,11 @@
 const express=require('express')
 const formateurRoute=express.Router()
-const {ajouter,liste} = require('../controllers/formateurController/formateur')
+const {ajouter,supprimer,update,liste} = require('../controllers/formateurController/formateur')
 
 formateurRoute.post('/ajouter-formateur',ajouter)
 formateurRoute.get('/liste-formateur',liste)
+formateurRoute.delete('/supprimer-formateur/:ids',supprimer)
+formateurRoute.put('/update-formateur/',update)
+
+
 module.exports=formateurRoute
